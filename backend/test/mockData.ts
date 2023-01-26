f5ee5870d6139c6d0e4c0d3058fb332eab0c6fbb
VMuser <vmuser@VMuser.myguest.virtualbox.org>
2023-01-26 02:23:15 -0800

    Commiting initial project files

---
+++ backend/test/mockData.ts
@@ -0,0 +1,23 @@
+const usersData = [{
+        id: 1,
+        name: 'name1',
+        email: 'email1',
+    },
+    {
+        id: 2,
+        name: 'name2',
+        email: 'email2',
+    },
+    {
+        id: 3,
+        name: 'name3',
+        email: 'email3',
+    },
+    {
+        id:4,
+        name: 'name4',
+        email: 'email4',
+    },
+]
+
+export {usersData};
